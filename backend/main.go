308ffa04f18b0f1091d77aa5481475c495bf4226
a5chin <capricorn42551230@gmail.com>
2022-04-01 04:22:06 +0900

    move model to model/db_model.go

--- backend/main.go
+++ backend/main.go
@@ -1,39 +1,18 @@
 package main
 
 import (
-	"fmt"
+	db_model "backend/model"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // TODO: fix type error
-type Question struct {
-	gorm.Model
-	year       int    `gorm:"not null"`
-	genre      string `gorm:"not null"`
-	question   string `gorm:"not null"`
-	answer     string `gorm:"not null"`
-	commentary string `gorm:"not null"`
-}
-
-func dbGetAll() []Question {
-	db, err := gorm.Open("sqlite3", "data/questions.db")
-	if err != nil {
-		panic("You can't open DB (dbGetAll())")
-	}
-	fmt.Print(db)
-	defer db.Close()
-	var questions []Question
-	db.Order("created_at desc").Find(&questions)
-	return questions
-}
 
 func main() {
 	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		questins := dbGetAll()
+	r.GET("/questions", func(c *gin.Context) {
+		questins := db_model.DBGetAll()
 		c.JSON(200, questins)
 	})
 	r.Run()
