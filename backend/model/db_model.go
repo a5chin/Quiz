62b47b7ed020f4983192f9a1ad810ba6c6b309a2
a5chin <capricorn42551230@gmail.com>
2022-04-01 04:23:15 +0900

    move model from main.go

---
+++ backend/model/db_model.go
@@ -0,0 +1,25 @@
+package db_model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Question struct {
+	gorm.Model
+	year       int    `gorm:"not null"`
+	genre      string `gorm:"not null"`
+	question   string `gorm:"not null"`
+	answer     string `gorm:"not null"`
+	commentary string `gorm:"not null"`
+}
+
+func DBGetAll() []Question {
+	db, err := gorm.Open("sqlite3", "data/questions.db")
+	if err != nil {
+		panic("You can't open DB (dbGetAll())")
+	}
+	defer db.Close()
+	var questions []Question
+	db.Order("created_at desc").Find(&questions)
+	return questions
+}
